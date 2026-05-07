ac601e09c35b52c7ab45e2c292de011c73093d59
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2026-05-07 11:19:26 +0200

    reworking some stuff

--- content/exercises/03_ex_feat_eng.py
+++ content/exercises/03_ex_feat_eng.py
@@ -98,10 +98,6 @@ encoder.fit_transform(df)
 #
 #
 
-# %% [markdown]
-# Now modify the script above to add spline features (`periodic_encoding="spline"`).
-#
-
 # %%
 # Solution
 from skrub import Cleaner
@@ -110,7 +106,7 @@ import skrub.selectors as s
 
 datetime_encoder = ApplyToCols(
     DatetimeEncoder(
-        periodic_encoding="spline",
+        periodic_encoding="circular",
         add_total_seconds=True,
         add_weekday=True,
         add_day_of_year=True,
@@ -121,4 +117,101 @@ datetime_encoder = ApplyToCols(
 encoder = make_pipeline(Cleaner(datetime_format="%d %B %Y"), datetime_encoder)
 encoder.fit_transform(df)
 
+# %% [markdown]
+# # Exercise
+# Build a custom `SingleColumnTransformer` that unpacks the combined string column
+# in the provided dataframe into separate columns for `str_id`, `num_id`, and
+# `datetime`. The `datetime` column should be converted to datetime dtype. Then,
+# use this transformer in a pipeline to extract datetime features as shown in
+# the previous exercises.
+#
+# The transformer should reject columns that are not of string type or that cannot 
+# be unpacked properly.
+# IDs are in the format `STR-NUM-DATETIME`, where `STR` is a string identifier, 
+# `NUM` is a numeric identifier, and `DATETIME` is a Unix timestamp.
+#
+# Hint: you can use the following snippet to extract the components from the string column:
+# ```python
+# split_data = X.str.split("-", expand=True)
+# res = pd.DataFrame(
+#     {
+#         "str_id": split_data[0],
+#         "num_id": split_data[1].astype("int64"),
+#         "datetime": pd.to_datetime(split_data[2].astype("int64"), unit="s"),
+#     }
+# )
+# ```
+
+# %%
+from skrub.core import SingleColumnTransformer, RejectColumn
+import pandas as pd
+from skrub import ApplyToCols
+df_id = pd.DataFrame(
+    {
+        "id": [
+            "BQG-1001-1577836800",
+            "TYW-1002-1577923200",
+            "JAY-1003-1578009600",
+        ]
+    }
+)
+# %%
+# Write your solution here
+#
+#
+#
+#
+#
+#
+#
+
+# %%
+# Solution
+class Unpacker(SingleColumnTransformer):
+    """Unpacker for pandas DataFrames."""
+
+    def fit_transform(self, X, y=None):
+        """Unpack combined string column into separate columns."""
+        if X.dtype != object:
+            raise RejectColumn("UnpackerPandas only works on string columns.")
+        try:
+            split_data = X.str.split("-", expand=True)
+            res = pd.DataFrame(
+                {
+                    "str_id": split_data[0],
+                    "num_id": split_data[1].astype("int64"),
+                    "datetime": pd.to_datetime(split_data[2].astype("int64"), unit="s"),
+                }
+            )
+            return res
+        except Exception as exc:
+            raise RejectColumn("UnpackerPandas failed to unpack the column.") from exc
+
+
+ApplyToCols(Unpacker(), allow_reject=True).fit_transform(df_id)
+
+# %% [markdown]
+# Now use this `Unpacker` in a pipeline to extract datetime features as shown in
+# the previous exercises. You can use the default `DatetimeEncoder` settings for
+# this part.
+
+# %%
+# Write your solution here
+#
+#
+#
+#
+#
+#
+#
+
+# %%
+from sklearn.pipeline import make_pipeline
+from skrub import DatetimeEncoder
+
+pipeline = make_pipeline(
+    ApplyToCols(Unpacker(), allow_reject=True),
+    ApplyToCols(DatetimeEncoder(), allow_reject=True),
+)
+pipeline.fit_transform(df_id)
 # %%
